a5738feab6f4d4a7c1d18db423663626c79a80a7
xwafflecakex <nyevtukh@gmail.com>
2018-01-17 21:01:50 -0800

    added the exception method after lab2
    basically from class

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ToolongException.java
@@ -0,0 +1,18 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by nynic on 2018-01-17.
+ */
+
+public class ToolongException extends Throwable {
+    /**
+     * The exception class if the tweet is too long.
+     */
+    public class TweetTooLongException extends Exception {
+        public TweetTooLongException() { }
+
+        public TweetTooLongException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+}
