abbd1bc6a7d4d4c97b88e96fad70fa29f82e4108
xwafflecakex <nyevtukh@gmail.com>
2018-01-17 20:54:37 -0800

    after lab2.
    Also used the override to replace the original tweet and date to the new one. Also included the throws to the too-long exception. Added getters' and setters' for tweets class

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,82 @@
+package ca.ualberta.cs.lonelytwitter;
+
+
+import java.util.Date;
+import java.util.ArrayList;
+
+/**
+ * Created by xwafflecakex on 2018-01-17.
+ */
+
+public class Tweet {
+    private Date date;
+    private String message;
+
+
+    /*
+    Takes a String message(tweet) and the date it was created and saves it.
+    Takes in a Date date and String message.
+    Throws an exception class that handles messages that are too long.
+     */
+    public Tweet(Date date, String message) throws TweetTooLongException {
+        this.setMessage(message);
+        this.date = date;
+    }
+
+    /*
+    Takes the String message(tweet) and automatically the current date on the device and saves it.
+    Takes in a String message.
+    Throws an exception class that handles messages that are too long.
+     */
+
+    public Tweet(String message) throws TweetTooLongException {
+        this.setMessage(message);
+        this.date = new Date(); // current time and date
+    }
+
+
+    /*
+    Gets the current date and returns it.
+    Returns the Date date.
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /*
+    Take the date from the string and set's it as the new date.
+    Takes in a Date date.
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /*
+    Gets the message from the string and returns it.
+    Returns a String message.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /*
+    Takes the message from the string and if it too long call TweetTooLongException
+    Takes in a String message.
+    Throws an exception class that handles messages that are too long.
+     */
+    public void setMessage(String message) throws TweetTooLongException {
+        if (message.length() > 145) {
+            throw new TweetTooLongException();
+        }
+        this.message = message;
+    }
+
+    /*
+    Overrides(replaces) the original string and returns it in a readable form
+     */
+    @Override
+    public String toString(){
+        return date.toString() + " | " + message;
+
+    }
+}
