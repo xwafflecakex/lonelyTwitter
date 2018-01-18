7afb90119cc8198c113ee2499b712db658a5344a
xwafflecakex <nyevtukh@gmail.com>
2018-01-17 21:08:39 -0800

    added tweetables after lab2
    This helps encapsulate tweets as well as providing polymorphism to tweet and  LonelyTwitterActivity

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetables.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+/**
+ * Created by nynic on 2018-01-17.
+ */
+
+public class Tweetables {
+    public String getMessage();
+    public Date getDate();
+}
