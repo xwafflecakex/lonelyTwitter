e89fadb7bdc4c1ac7dc0299559709d7584074dd8
xwafflecakex <nyevtukh@gmail.com>
2018-01-18 16:47:03 -0800

    Added Comments
    Found how to use auto-gen comments

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -7,15 +7,29 @@ import java.util.Date;
  */
 
 public class NormalTweet extends Tweet {
+    /**
+     *
+     * @param message the tweet message
+     * @throws ToolongException if the tweet message is too long
+     */
     NormalTweet(String message) throws ToolongException {
         super(message);
 
     }
-
+    /**
+     *
+     * @param message the tweet message
+     * @param date  the current date
+     * @throws ToolongException if the tweet message is too long
+     */
     NormalTweet(String message, Date date) throws ToolongException {
         super(date, message);
     }
-
+    
+    /**
+     *
+     * @return false.
+     */
     @Override
     public boolean isImportant(){
         return false;
