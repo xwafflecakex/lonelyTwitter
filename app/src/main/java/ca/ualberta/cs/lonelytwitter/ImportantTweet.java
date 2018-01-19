c8156bbd3af8742e6e08e0f55407307501804aa3
xwafflecakex <nyevtukh@gmail.com>
2018-01-18 16:49:34 -0800

    added comments

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -7,14 +7,27 @@ import java.util.Date;
  */
 
 public class ImportantTweet extends Tweet{
+    /**
+     *
+     * @param message the tweet message
+     * @throws ToolongException if the tweet message is too long
+     */
     ImportantTweet(String message) throws ToolongException {
         super(message);
     }
-
+    /**
+     *
+     * @param message the tweet message
+     * @param date the current date
+     * @throws ToolongException if the tweet message is too long
+     */
     ImportantTweet(String message, Date date) throws ToolongException {
         super(date, message);
     }
-
+    /**
+     *
+     * @return true
+     */
     @Override
     public boolean isImportant(){
         return true;
