ee371112e3f64d070b2f93ad1c60902b4e7a976e
xwafflecakex <nyevtukh@gmail.com>
2018-01-24 20:59:47 -0800

    Updated to lab3  part
    Added GSON serialization and deserialization of memory storage of class objects in java. this retains the tweets apon closing the app.

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -17,6 +17,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class LonelyTwitterActivity extends Activity {
 
@@ -25,6 +27,9 @@ public class LonelyTwitterActivity extends Activity {
 	private ListView oldTweetsList;
 	
 	/** Called when the activity is first created. */
+	private ArrayList<Tweet> tweetlist; //to visually see the tweetlist later on.
+	private ArrayAdapter<Tweet> adapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +37,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -40,55 +46,32 @@ public class LonelyTwitterActivity extends Activity {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
 
-				NormalTweet newtweet = null;
+				Tweet newtweet = null;
 				try {
 					newtweet = new NormalTweet(text);
 				} catch (ToolongException e) {
 					e.printStackTrace();
 				}
-				NormalTweet newtweet2 = null;
-				try {
-					newtweet2 = new NormalTweet(text, new Date());
-				} catch (ToolongException e) {
-					e.printStackTrace();
-				}
 
-				ImportantTweet imptweet = null;
-				try {
-					imptweet = new ImportantTweet("this is a important tweet");
-				} catch (ToolongException e) {
-					e.printStackTrace();
-				}
+				tweetlist.add(newtweet);
 
-				NormalTweet normtweet = null;
-				try {
-					normtweet = new NormalTweet("this is a normal tweet");
-				} catch (ToolongException e) {
-					e.printStackTrace();
-				}
+				adapter.notifyDataSetChanged();
 
-				ArrayList<Tweet> alltweets = new ArrayList<Tweet>();
-				alltweets.add(newtweet);
-				alltweets.add(newtweet2);
-				alltweets.add(imptweet);
-				alltweets.add(normtweet);
+				saveInFile();
 
-				// write it to the log , just to check outputs
-				Log.v("Test", "Log error 1");
+			}
+		});
+		
+		clearButton.setOnClickListener(new View.OnClickListener() {
 
+			public void onClick(View v) {
+				setResult(RESULT_OK);
 
-				try{
-					newtweet.setMessage("Message too long");
+				tweetlist.clear();
 
-				}
-				catch(Exception e){
-					// Show a error message
-					e.printStackTrace();
-				}
-				
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				adapter.notifyDataSetChanged();
 
+				saveInFile();
 			}
 		});
 	}
@@ -97,40 +80,48 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+		loadFromFile();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetlist);
 		oldTweetsList.setAdapter(adapter);
 	}
 
 	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+
+			Gson gson = new Gson();
+
+			//Taken https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+			// 2018-01-24
+
+			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+
+			tweetlist = gson.fromJson(in, listType);
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			tweetlist = new ArrayList<Tweet>();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+
 	}
 	
 	private void saveInFile(String text, Date date) {
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write((date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+					Context.MODE_PRIVATE);
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+			Gson gson = new Gson();
+
+			gson.toJson(tweetlist, out);
+
+			out.flush();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -138,5 +129,11 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	/**
+	  * Testing the destroy memory on closing of the app.
+	protected void onDestroy(){
+		super.onDestroy();
+		Log.i("In Destroy method","The app is closing");
 	}
 }
