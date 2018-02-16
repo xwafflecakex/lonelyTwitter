c27e861afae36d62b31f30c6ce36f00345c01b0e
xwafflecakex <nyevtukh@gmail.com>
2018-02-16 10:36:46 -0700

    Update TweetList.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,29 +1,46 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Created by watts1 on 1/28/16.
+ * Edited by yevtukh on 16/02/18.
  */
-public class TweetList {
-    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+public class TweetList extends Exception {
+    private ArrayList<Tweet> tweets = new ArrayList<>();
 
-    public void add(Tweet tweet){
-        tweets.add(tweet);
+    public TweetList(){
+
     }
 
     public boolean hasTweet(Tweet tweet){
         return tweets.contains(tweet);
     }
 
-    public void delete(Tweet tweet){
-        tweets.remove(tweet);
+    public void add(Tweet tweet)throws IllegalArgumentException{
+        if(!hasTweet(tweet)) {
+            tweets.add(tweet);
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
+
 
     public Tweet getTweet(int index){
         return tweets.get(index);
     }
 
+    public void delete(Tweet tweet){
+        tweets.remove(tweet);
+    }
 
+    public int getCount(){
+        return tweets.size();
+    }
 
+    public ArrayList<Tweet> getTweets(){
+        return tweets;
+    }
 }
